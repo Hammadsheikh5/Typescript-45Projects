4c6eab0a380d1bd7a9808147896b68c03eb5733a
Hammadsheikh5 <mhammadkhi5@gmail.com>
2024-03-07 17:47:33 +0500

    first commit

---
+++ task1/demo.js
@@ -0,0 +1,2 @@
+var PersonName = "HAMMAD";
+console.log("Hello", PersonName, "Would you like to learn some Python today? ");
---
+++ task1/demo.ts
@@ -0,0 +1,2 @@
+let PersonName : string = "HAMMAD";
+console.log("Hello", PersonName ,"Would you like to learn some Python today? ");
