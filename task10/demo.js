c868fa9565daa48c31ee8a8b63b86285639adaf1
Hammadsheikh5 <mhammadkhi5@gmail.com>
2024-03-07 18:37:31 +0500

    first commit'

---
+++ task10/demo.js
@@ -0,0 +1,7 @@
+//Program by Hammad Sheikh 
+//Date: 25-02-2024
+//Simple Addition\ Subtaction\ Multiplication\ Division Of Two Numbers
+console.log("ADDITION : " + (5 + 3));
+console.log("SUBTRACTION : " + (11 - 3));
+console.log("MULTIPLICATION : " + (4 * 2));
+console.log("DIVISION : " + (24 / 3));
