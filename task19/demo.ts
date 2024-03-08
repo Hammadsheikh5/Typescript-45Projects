84d9b89699cc843107836484b67f5d2fa2cdefed
Hammadsheikh5 <mhammadkhi5@gmail.com>
2024-03-08 11:56:00 +0500

    second commit

--- task19/demo.ts
+++ task19/demo.ts
@@ -4,6 +4,6 @@ let guestList: string[] = ["Hamza", "Usman", "Ali", "Saad"];
 guestList.forEach(guest =>{
     console.log(`Assalam-u-Alaikum My Dear ${guest}, I would like to invite you to dinner with me`)});
 
-// Inform about the number of ~ invited to dinner
+// Inform about the number of guests invited to dinner
 let guestlength : number = guestList.length
 console.log(`\nI am inviting ${guestlength} people to dinner.`);
