881b7bc496111453bb6bc9b1bd320a33351269eb
Hammadsheikh5 <mhammadkhi5@gmail.com>
2024-03-11 11:48:39 +0500

    first commit

---
+++ task23/demo.ts
@@ -0,0 +1,48 @@
+// declare variables for test
+let a : number = 9;
+let b : number = 5;
+let Myname : string = "Hammad"
+let Colour : string = "Black";
+let Fruit : string = "Apple";
+let count : string = "Six";
+
+
+// Test 1:
+console.log("Is a greater than or equal to 6? I predict True.");
+console.log(a >= 6);
+
+// Test 2: 
+console.log("Is a equal to 9? I predict True.");
+console.log(a == 9);
+
+// Test 3: 
+console.log("Is a not equal to b? I predict True.");
+console.log(a != b);
+
+// Test 4:
+console.log("Is a equal to b? I predict False.");
+console.log(a == b);
+
+// Test 5:
+console.log("Is a less than b? I predict False.");
+console.log(a < b);
+
+// Test 6: 
+console.log("Is b greater than 11 ? I predict False.");
+console.log(b > 11);
+
+// Test 7: 
+console.log("Is colour strictly equal to Blue? I predict False.");
+console.log(Colour === 'Blue');
+
+// Test 8:
+console.log("Is Fruit loosely equal to banana? I predict False.");
+console.log(Fruit == 'banana');
+
+// Test 9: 
+console.log("Is count equal to Six? I predict True.");
+console.log(count == 'Six');
+
+// Test 10:
+console.log("Is Myname is hammmad? I predict True.");
+console.log(Myname == 'Hammad');
