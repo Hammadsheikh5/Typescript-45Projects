7be67287a9b05efed2504460a8331f10abced717
Hammadsheikh5 <mhammadkhi5@gmail.com>
2024-03-13 10:45:19 +0500

    first commit

---
+++ task36/demo.js
@@ -0,0 +1,4 @@
+function make_shirt(size, message) {
+    console.log("You selected \"".concat(size, "\" size shirt with the \"").concat(message, "\" prints on it."));
+}
+make_shirt("Medium", "Goodluck");
